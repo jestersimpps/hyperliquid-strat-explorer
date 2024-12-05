5d56e59cfab92d27fc9b2ced4094d05b6e611332
Jo Vinkenroye <jestersimpps@gmail.com>
2024-12-05 21:45:12 +0100

    feat: Subscribe to BTC 1-minute candles in index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,18 @@ async function main() {
             console.log('Ticker Update:', ticker);
         });
 
+        // Subscribe to BTC 1-minute candles
+        await wsApi.subscribeToCandles('BTC', '1m', (candle) => {
+            console.log(`[${new Date(candle.t).toISOString()}] BTC Candle:`, {
+                open: candle.o,
+                high: candle.h,
+                low: candle.l,
+                close: candle.c,
+                volume: candle.v,
+                trades: candle.n
+            });
+        });
+
         // If you have user authentication set up
         // await wsApi.subscribeToUserEvents((event) => {
         //     console.log('User Event:', event);
