66b82b0beba1e90c4324c312ec1721f3422f6c0b
Jo Vinkenroye <jestersimpps@gmail.com>
2024-12-14 15:57:55 +0100

    refactor: Refactor symbol display updater to use shared updater functions

--- src/ui/symbol-display-updater.ts
+++ src/ui/symbol-display-updater.ts
@@ -1,1 +1,18 @@
-export { updateBreakoutBox, updateChart } from './shared-updater';
+import * as contrib from "blessed-contrib";
+import { BreakoutSignal } from "../types/breakout";
+import { updateBreakoutBox as sharedUpdateBreakoutBox, updateChart as sharedUpdateChart } from './shared-updater';
+
+export function updateBreakoutBox(
+  breakoutBox: contrib.Widgets.TableElement,
+  breakoutSignals: Map<string, BreakoutSignal>
+): void {
+  sharedUpdateBreakoutBox(breakoutBox, breakoutSignals);
+}
+
+export function updateChart(
+  chart: contrib.Widgets.LineElement,
+  data: { times: string[]; prices: number[]; support: number[]; resistance: number[] },
+  symbol: string
+): void {
+  sharedUpdateChart(chart, data);
+}
